9be904330775557a932e28f7f3ff4e7796454f6b
SK3620 <k.n.t11193620@gmail.com>
2025-05-08 01:21:45 +0900

    add: actorのIsolation Domainと非Sendable型によるコンパイルエラーを学習

---
+++ Practice-Concurrency-App.playground/Pages/Isolation-Boundary.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+final class Box {
+    var value: Int = 0
+}
+
+actor A {
+    var box: Box?
+    func setBox(_ box: Box) {
+        self.box = box
+    }
+}
+
+func run1() {
+    let box = Box()
+    let a: A = .init() // A の Isolation Domain
+    
+    //  A の Isolation Domain に Box を渡そうしている
+    // すなわち、Box は A の Isolation Boundary を越えようとしている
+    // しかし、Box は Sendable でないため、コンパイルエラー発生！
+    a.setBox(box) // Non-Sendable なデータが Isolation Boundary を越えようとしているよ！
+}
+
+func run2() {
+    let box = Box()
+    
+    Task {
+        box.value = -1
+    }
+
+    Task {
+        print(box.value)
+    }
+}
+
+run1()
+run2()
+
+

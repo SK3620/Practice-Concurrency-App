bff9e4514af80c1348e2b3106fda3a267e64e589
SK3620 <k.n.t11193620@gmail.com>
2025-05-07 22:56:00 +0900

    add: MainActor外のTaskでのデータ競合発生の可能性を学ぶサンプルを追加

---
+++ Practice-Concurrency-App.playground/Pages/Actor.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+final class Box {
+    var value: Int = 0
+}
+
+let box = Box()
+
+// 実行時、MainActorではない時はクラッシュさせる
+MainActor.assertIsolated()
+
+// 以下は並行処理
+// そして、Taskは actor Context を引き継ぐ
+Task {
+    // MainActor
+    MainActor.assertIsolated()
+    box.value = -1
+}
+
+Task {
+    // MainActor
+    MainActor.assertIsolated()
+    print(box.value)
+}
+
+// 両者、MainActor（一つのスレッド）のため、box.value = -1 と print(box.value) が同時に実行されることはなく、一つ一つ順番に実行される
+// よって、コンパイルが通る
+
+

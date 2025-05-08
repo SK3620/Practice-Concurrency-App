ccb780c2e5a9ec9e1a96d2b165253779c5510cbb
SK3620 <k.n.t11193620@gmail.com>
2025-05-08 11:22:41 +0900

    update: classインスタンスの Isolation Domain 領域内のプロパティやメソッドへのアクセスは await が必要

--- Practice-Concurrency-App.playground/Pages/Isolation-Boundary.xcplaygroundpage/Contents.swift
+++ Practice-Concurrency-App.playground/Pages/Isolation-Boundary.xcplaygroundpage/Contents.swift
@@ -17,29 +17,29 @@ actor A {
     }
 }
 
-func run1() {
+func run1() async {
     let box = Box()
     let a: A = .init() // A の Isolation Domain
     
     //  A の Isolation Domain に Box を渡そうしている
     // すなわち、Box は A の Isolation Boundary を越えようとしている
     // しかし、Box は Sendable でないため、コンパイルエラー発生！
-    a.setBox(box) // Non-Sendable なデータが Isolation Boundary を越えようとしているよ！
+    await a.setBox(box) // Non-Sendable なデータが Isolation Boundary を越えようとしているよ！
 }
 
 func run2() {
     let box = Box()
     
-    Task {
+    /*
+    Task { // バックグラウンドスレッドA
         box.value = -1
     }
 
-    Task {
+    Task { // バックグラウンドスレッドB
         print(box.value)
     }
+     */
 }
 
-run1()
+await run1()
 run2()
-
-
